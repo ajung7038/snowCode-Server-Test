454287957bda227fb7506d0daf361e1370090d7e
ajeong <ajung7038@naver.com>
2025-07-02 19:12:08 +0900

    Feat: 초기 세팅

---
+++ src/main/java/snowcode/snowcode/SnowcodeApplication.java
@@ -0,0 +1,13 @@
+package snowcode.snowcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snowcode/snowcode/SnowcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package snowcode.snowcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
