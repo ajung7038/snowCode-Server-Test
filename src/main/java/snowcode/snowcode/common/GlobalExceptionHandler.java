6ecc455a9a477a8543d0e72e3c7ad7f4fe9a076a
ajeong <ajung7038@naver.com>
2025-07-12 21:24:45 +0900

    Feat: 날짜, Exception 기본 세팅 #2

---
+++ src/main/java/snowcode/snowcode/common/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package snowcode.snowcode.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import snowcode.snowcode.common.response.BasicResponse;
+import snowcode.snowcode.common.response.ErrorEntity;
+import snowcode.snowcode.common.response.ResponseUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public BasicResponse<ErrorEntity> validationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+                .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+        log.error("Dto Validation Exception({}) : {}", "BAD_INPUT", errors);
+        return ResponseUtil.error(new ErrorEntity("BAD_INPUT", "입력이 올바르지 않습니다.", errors));
+    }
+}
---
+++ src/main/java/snowcode/snowcode/common/response/BasicResponse.java
@@ -0,0 +1,11 @@
+package snowcode.snowcode.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BasicResponse<T> {
+    private boolean success;
+    private T response;
+}
