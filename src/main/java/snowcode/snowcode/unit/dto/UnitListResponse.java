50be3008ff8248c601424daf64688db0f3aa296d
ajeong <ajung7038@naver.com>
2025-07-20 09:00:09 +0900

    Fix: 단원 전체 조회 로직 수정 #21

--- src/main/java/snowcode/snowcode/unit/dto/UnitListResponse.java
+++ src/main/java/snowcode/snowcode/unit/dto/UnitListResponse.java
@@ -2,9 +2,9 @@ package snowcode.snowcode.unit.dto;
 
 import snowcode.snowcode.unit.domain.Unit;
 
-public record UnitListResponse(String title) {
+public record UnitListResponse(Long id, String title, String releaseDate, String dueDate) {
 
     public static UnitListResponse from (Unit unit) {
-        return new UnitListResponse(unit.getTitle());
+        return new UnitListResponse(unit.getId(), unit.getTitle(), unit.getReleaseDate().toString(), unit.getDueDate().toString());
     }
 }
