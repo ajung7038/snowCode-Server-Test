b3f7f77468a052a4da6d60bb568733b3f8f8be38
ajeong <ajung7038@naver.com>
2026-01-26 08:54:48 +0900

    Fix: Poller 스레드 개수 변경 제거

--- src/main/java/snowcode/snowcode/common/config/TomcatCustomConfig.java
+++ src/main/java/snowcode/snowcode/common/config/TomcatCustomConfig.java
@@ -6,20 +6,20 @@ import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerF
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class TomcatCustomConfig {
 
-    @Bean
-    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
-        return factory -> {
-            if (factory instanceof TomcatServletWebServerFactory tomcat) {
-                tomcat.addConnectorCustomizers(connector -> {
-                    // Poller 스레드 개수를 2개로 설정 (기본값 1)
-                    connector.setProperty("pollerThreadCount", "2");
-//                    // selectorTimeout을 5000ms(5초)로 변경 -> 0.5초로 변경
-//                    connector.setProperty("selectorTimeout", "2000");
-                });
-            }
-        };
-    }
+//    @Bean
+//    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+//        return factory -> {
+//            if (factory instanceof TomcatServletWebServerFactory tomcat) {
+//                tomcat.addConnectorCustomizers(connector -> {
+//                    // Poller 스레드 개수를 2개로 설정 (기본값 1)
+//                    connector.setProperty("pollerThreadCount", "2");
+////                    // selectorTimeout을 5000ms(5초)로 변경 -> 0.5초로 변경
+////                    connector.setProperty("selectorTimeout", "2000");
+//                });
+//            }
+//        };
+//    }
 }
