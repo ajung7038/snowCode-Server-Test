95c6fdc4b1fd49f90fd1b0fffb5680b5d28f65ba
ajeong <ajung7038@naver.com>
2026-02-01 12:33:30 +0900

    Fix: 스레드 풀 크기 늘리기

--- src/main/java/snowcode/snowcode/common/config/AsyncConfig.java
+++ src/main/java/snowcode/snowcode/common/config/AsyncConfig.java
@@ -1,11 +1,10 @@
 package snowcode.snowcode.common.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.context.request.async.StandardServletAsyncWebRequest;
-import org.apache.tomcat.util.net.NioEndpoint;
 
 @Configuration
 @EnableAsync
@@ -15,9 +14,9 @@ public class AsyncConfig {
         // 테스크 수행시 스레드풀 사용
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         // 유지할 풀 사이즈
-        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setCorePoolSize(150);
         // 최대 풀 사이즈
-        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setMaxPoolSize(200);
         // 스레드 종료까지 시간
         taskExecutor.setAwaitTerminationSeconds(30);
         // 스레드 네임 프리픽스 설정
